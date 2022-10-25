e51b5f88c379c866e83fcb66dba6b35c0443efc8
akriti-star <sharmaakriti8678@gmail.com>
2022-10-25 19:36:27 +0530

    practice commit

---
+++ Desktop/PYTHON/break_conti.py
@@ -0,0 +1,13 @@
+students=["ram","sham","radha","sita"]
+for student in students:
+    if student =="radha":
+        break;
+    print(student)
+
+
+students=["ram","sham","radha","sita"]
+for student in students:
+    if student =="radha":
+        continue;
+    print(student)
+       
