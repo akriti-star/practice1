0302cf213538b0790e707730cd69589a2a9d7070
akriti-star <sharmaakriti8678@gmail.com>
2022-10-26 23:31:04 +0530

    learning python

---
+++ Desktop/PYTHON/break-conti.py
@@ -0,0 +1,12 @@
+students=["ram","sham","sita","gita","kishan"]
+for i in students:
+    if i=="sita":
+        break;
+    print (i)
+
+
+students=["ram","sham","sita","gita","kishan"]
+for i in students:
+    if i=="sita":
+        continue;
+    print (i)
