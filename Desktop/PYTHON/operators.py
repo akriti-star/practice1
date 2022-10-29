9b0548ed996ed4af166946b56cd9d9579253a84b
akriti-star <sharmaakriti8678@gmail.com>
2022-10-29 22:34:12 +0530

    learning python

---
+++ Desktop/PYTHON/operators.py
@@ -0,0 +1,24 @@
+#assignment operators
+a=6
+a+=4
+a-=5
+a*=6
+a/=7
+print(a)
+#comparision operators
+a=(13>=8)
+print(a)
+b=(4==5)
+print(b)
+c=(4<=1)
+print(c)
+d=(3>2)
+print(d)
+e=(3!=4)
+print(e)
+#logical operators
+bool1 = True
+bool2 = False
+print("print the value of bool1 and bool2:",(bool1 and bool2))
+print("print the value of bool1 and bool2:",(bool1 or bool2))
+print("print the value of bool1 and bool2:",(not bool2))
