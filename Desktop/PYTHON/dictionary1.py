be6245deb392e8c0644d211b2e68f0ed976717a5
akriti-star <sharmaakriti8678@gmail.com>
2022-12-04 13:17:38 +0530

    file

---
+++ Desktop/PYTHON/dictionary1.py
@@ -0,0 +1,8 @@
+data={"name":"Akriti","Age":18,"Class":10+2,"Percentage":"79.0%"}
+
+print(data["Age"])
+data["Age"]=34
+print(data)
+print(data.keys())
+print(data.values())
+print(data.pop("name"))
