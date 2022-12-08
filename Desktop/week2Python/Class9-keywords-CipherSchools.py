116270ce51b218e1dd56ded971d4e1f6577558b7
akriti-star <sharmaakriti8678@gmail.com>
2022-12-08 19:35:51 +0530

    project

---
+++ Desktop/week2Python/Class9-keywords-CipherSchools.py
@@ -0,0 +1,32 @@
+x=9
+x='Akriti'
+x=1.5
+x=print
+def show_loading():
+    for i in range(3):
+        print("loading")
+a=5
+b=7
+# print(a+b)
+# print("loading")
+# print("loading")
+# print("loading")
+
+# print(a-b)
+# print("loading")
+# print("loading")
+# print("loading")
+
+# print(a*b)
+# print("loading")
+# print("loading")
+# print("loading")
+
+print(a+b)
+show_loading()
+
+print(a-b)
+show_loading()
+
+print(a*b)
+show_loading()
