241eea6a55900926113eeb101421d9f53737e41f
akriti-star <sharmaakriti8678@gmail.com>
2022-12-08 19:43:47 +0530

    project

---
+++ Desktop/week2Python/Class9-FunctionAndReturnStatement-CipherSchools.py
@@ -0,0 +1,9 @@
+def sheldon_knock(name):
+    for _ in range(3):
+        print("knock knock knock", name)
+sheldon_knock("leonard")
+
+def sheldon_knock (me,times=3):
+    for _ in range(times):
+        print("knock knock knock",me)
+sheldon_knock("penny",100)
